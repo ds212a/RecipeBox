08ea3d1a32044a8575f2c583a345820a5e220d42
Christopher Jett <ds212@hotmail.com>
2021-03-17 20:24:12 -0700

    Add IngredientToStringConverter.

---
+++ RecipeBox/Converters/IngredientToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace RecipeBox.Converters
+{
+    public class IngredientToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Ingredient ingredient = (Ingredient)value;
+            string formatted = string.Empty;
+
+            if (ingredient.UnitOfMeasurement == Ingredient.UnitOfMeasurements.Header)
+            {
+                formatted = $"{ingredient.Name}";
+            }
+            else
+            {
+                formatted = $"\u2022 {ingredient}";
+            }
+
+            return formatted;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
